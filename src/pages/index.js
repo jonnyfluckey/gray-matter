49ff7586b2232f2a97a34290f0a123d650dc71a5
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-12-31 14:51:08 -0700

    add react helmet for SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
-
+import { Helmet } from 'react-helmet'
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
+import Image from '../images/brain-main.jpg'
 
 class IndexPage extends React.Component {
   constructor(props) {
@@ -13,101 +14,117 @@ class IndexPage extends React.Component {
       timeout: false,
       articleTimeout: false,
       article: '',
-      loading: 'is-loading'
+      loading: 'is-loading',
     }
     this.handleOpenArticle = this.handleOpenArticle.bind(this)
     this.handleCloseArticle = this.handleCloseArticle.bind(this)
-    this.setWrapperRef = this.setWrapperRef.bind(this);
-    this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.setWrapperRef = this.setWrapperRef.bind(this)
+    this.handleClickOutside = this.handleClickOutside.bind(this)
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.timeoutId = setTimeout(() => {
-        this.setState({loading: ''});
-    }, 100);
-    document.addEventListener('mousedown', this.handleClickOutside);
+      this.setState({ loading: '' })
+    }, 100)
+    document.addEventListener('mousedown', this.handleClickOutside)
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     if (this.timeoutId) {
-        clearTimeout(this.timeoutId);
+      clearTimeout(this.timeoutId)
     }
-    document.removeEventListener('mousedown', this.handleClickOutside);
+    document.removeEventListener('mousedown', this.handleClickOutside)
   }
 
   setWrapperRef(node) {
-    this.wrapperRef = node;
+    this.wrapperRef = node
   }
 
   handleOpenArticle(article) {
-
     this.setState({
       isArticleVisible: !this.state.isArticleVisible,
-      article
+      article,
     })
 
     setTimeout(() => {
       this.setState({
-        timeout: !this.state.timeout
+        timeout: !this.state.timeout,
       })
     }, 325)
 
     setTimeout(() => {
       this.setState({
-        articleTimeout: !this.state.articleTimeout
+        articleTimeout: !this.state.articleTimeout,
       })
     }, 350)
-
   }
 
   handleCloseArticle() {
-
     this.setState({
-      articleTimeout: !this.state.articleTimeout
+      articleTimeout: !this.state.articleTimeout,
     })
 
     setTimeout(() => {
       this.setState({
-        timeout: !this.state.timeout
+        timeout: !this.state.timeout,
       })
     }, 325)
 
     setTimeout(() => {
       this.setState({
         isArticleVisible: !this.state.isArticleVisible,
-        article: ''
+        article: '',
       })
     }, 350)
-
   }
 
   handleClickOutside(event) {
     if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
       if (this.state.isArticleVisible) {
-        this.handleCloseArticle();
+        this.handleCloseArticle()
       }
     }
   }
 
   render() {
     return (
-      <Layout location={this.props.location}>
-        <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
-          <div id="wrapper">
-            <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
-            <Main
-              isArticleVisible={this.state.isArticleVisible}
-              timeout={this.state.timeout}
-              articleTimeout={this.state.articleTimeout}
-              article={this.state.article}
-              onCloseArticle={this.handleCloseArticle}
-              setWrapperRef={this.setWrapperRef}
-            />
-            <Footer timeout={this.state.timeout} />
+      <>
+        <Helmet>
+          <meta
+            property="og:description"
+            content="Digitally Transform your Data and Processes to Scale your Business"
+          />
+          <meta property="og:image" content={Image} />
+          <meta
+            property="og:title"
+            content="Grow your Business Through Technology | Gray Matter Tech"
+          />
+        </Helmet>
+        <Layout location={this.props.location}>
+          <div
+            className={`body ${this.state.loading} ${
+              this.state.isArticleVisible ? 'is-article-visible' : ''
+            }`}
+          >
+            <div id="wrapper">
+              <Header
+                onOpenArticle={this.handleOpenArticle}
+                timeout={this.state.timeout}
+              />
+              <Main
+                isArticleVisible={this.state.isArticleVisible}
+                timeout={this.state.timeout}
+                articleTimeout={this.state.articleTimeout}
+                article={this.state.article}
+                onCloseArticle={this.handleCloseArticle}
+                setWrapperRef={this.setWrapperRef}
+              />
+              <Footer timeout={this.state.timeout} />
+            </div>
+            <div id="bg"></div>
           </div>
-          <div id="bg"></div>
-        </div>
-      </Layout>
+        </Layout>
+      </>
     )
   }
 }
